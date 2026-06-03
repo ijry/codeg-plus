138651eaea0b872b6971bc59db88fbfc7adedfa3
xintaofei <itpkcn@gmail.com>
2026-06-03 18:50:22 +0800

    style(message): tighten list left padding in markdown response

--- src/components/ai-elements/message.tsx
+++ src/components/ai-elements/message.tsx
@@ -389,7 +389,7 @@ function MessageResponseImpl({
   return (
     <Streamdown
       className={cn(
-        "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0 [&_ul]:list-disc [&_ol]:list-decimal [&_ul]:pl-4 [&_ol]:pl-4",
+        "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0 [&_ul]:list-disc [&_ol]:list-decimal [&_ul]:pl-3 [&_ol]:pl-3",
         className
       )}
       linkSafety={linkSafety}
